1a04c5336abd287db35c42c846baca32ec8607f8
drishtantr <drishtantregmi777@gmail.com>
2020-04-03 08:58:52 +0545

    7

---
+++ app/Post.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace App;
+
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\SoftDeletes;
+
+class Post extends Model
+{
+    //
+
+
+    protected $fillable=[
+        'title',
+        'content'
+
+
+    ];
+}
--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,5 @@
 <?php
-
+use App\Post;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -39,11 +39,11 @@ use Illuminate\Support\Facades\Route;
 
 // SQL insert update delete etc etc from here
 
-// route::get('/insert',function(){
+route::get('/insert',function(){
 
-//     DB::insert('Insert into posts(title,content) values(?,?)',['Apple','Ball']);
+    DB::insert('Insert into posts(title,content) values(?,?)',['Php2','Laravel2']);
 
-// });
+});
 
 
 // route::get('/read',function(){
@@ -70,4 +70,99 @@ use Illuminate\Support\Facades\Route;
 //     return $deleted;
  
 //  });
+
+// Eloquent from here
+
+// Route::get('/read',function(){
+
+//     $posts=Post::all();
+//     foreach($posts as $post){
+
+//         return $post->title;
+//     }
+
+// });
+
+// Route::get('/find',function(){
+
+//     $post=Post::find(2);
+
+//     return $post->title;
+//     // foreach($posts as $post){
+
+//     //     return $post->title;
+//     // }
+
+// });
+
+// Route::get('/findwhere',function(){
+
+//     $posts=Post::where('id',3)->orderBy('id','desc')->take(1)->get();
+//     return $posts;
+     
+
+// });
+
+// Route::get('/findmore',function(){
+
+//     $posts=Post::where('id',3)->orderBy('id','desc')->take(1)->get();
+//     return $posts;
+     
+
+// });
+
+// Route::get('/binsert',function(){
+
+//    $post=new Post;
+//    $post->title="php3";
+//    $post->content="laravel3";
+
+   
+//    $post->save();
+
+// });
+
+
+
+// Route::get('/bupdate',function(){
+
+//     $post=Post::find(2);
+//     $post->title="php1";
+//     $post->content="laravel1";
+ 
+    
+//     $post->save();
+ 
+//  });
+
+// Route::get('/create',function(){
+//     Post::create(['title'=>'Create PHP','content'=>'Create Laravel']);
+
+
+// });
+
+// Route::get('/equpdate',function(){
+//     Post::where('id',2)->where('is_admin',1)->update(['title'=>'DICK','content'=>'LARAVEL1']);
+
+
+// });
+
+// Route::get('/bdelete',function(){
+
+//     $post=Post::find(2);
+  
+    
+//     $post->delete();
+ 
+//  });
+
+//  Route::get('/bdelete2',function(){
+
+//     Post::destroy([4,5]);
+
+ 
+//  });
+
+
+
 
